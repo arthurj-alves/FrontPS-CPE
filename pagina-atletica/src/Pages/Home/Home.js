538415d74010e75af7acf48d72f01a6b7ec756aa
Bruno Cimbleris <bcimbleris@gmail.com>
2022-10-27 20:59:10 -0300

    adiciona loginbox, background color e conserta bugs de aparencia do login

--- pagina-atletica/src/Pages/Home/Home.js
+++ pagina-atletica/src/Pages/Home/Home.js
@@ -7,13 +7,13 @@ function Home() {
 
   return (
     <div>
-      <h1>Home</h1>;
+      <h1>Home</h1>
       <button 
         onClick={() => {
           Navigate('/login');
         }}
       >
-        Logout
+        Login
       </button>
     </div>
   )
